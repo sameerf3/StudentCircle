17526b6678db5a26f9496cb0d1f0441caf5b4064
Sameer Ahmed Siddiqui <sameerdgrea8@yahoo.com>
2014-03-19 14:05:58 +0500

    Moving project to Git

---
+++ StudentCircle/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentCircle::Application.config.secret_key_base = 'ccf301238795aa799f0f980c84608bb024e33c56bc02bceb75dbb785069cb78ead20bc44ce07c76ffb55e673430820980c152238ebdc688c7b25a23391e0f526'
